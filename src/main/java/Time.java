c7519b8c0094fcb2ec5bcf4a4474306706a618da
chul1ta26 <dayhany26@gmail.com>
2019-09-23 23:27:46 -0400

    part 3

--- src/main/java/Time.java
+++ src/main/java/Time.java
@@ -1,5 +1,14 @@
 public class Time {
     public static void main (String [] arg){
-        System.out.println("practice");
+
+      // exercise 2.3
+      //part 2
+        double hour = 23;
+        double minute = 18;
+       double second = 35;
+
+       // part 3
+        double result = (hour * 3600) + (minute * 60) + second;
+        System.out.println( "Seconds since midnight: \n" + result);
     }
 }
